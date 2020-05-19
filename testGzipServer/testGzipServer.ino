0057dc18f53e90508ac72d817ea34529922eca49
GelatoBaboom <idx001@gmail.com>
2020-05-19 13:33:16 -0300

    softap +captive

--- testGzipServer/testGzipServer.ino
+++ testGzipServer/testGzipServer.ino
@@ -4,15 +4,16 @@
 //#include <ESP8266WebServer.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncTCP.h>
-#include <ESP8266mDNS.h>
+//#include <ESP8266mDNS.h>
+#include <DNSServer.h>
 #include "index.h"
-
+IPAddress apIP(192, 168, 4, 1);
 #define DBG_OUTPUT_PORT Serial
 
 const char* ssid = "GelatoBaboom";
 const char* password = "friofrio";
 const char* host = "esp8266sd";
-
+DNSServer dnsServer;
 AsyncWebServer  server = AsyncWebServer(80);
 
 void temp_handler(AsyncWebServerRequest *request) {
@@ -24,7 +25,7 @@ void temp_handler(AsyncWebServerRequest *request) {
   *p++ = '}';
   *p++ = 0;
 
-  AsyncWebServerResponse *response = request->beginResponse(200,"application/json", json_response);
+  AsyncWebServerResponse *response = request->beginResponse(200, "application/json", json_response);
   response->addHeader("Access-Control-Allow-Origin", "*");
   request->send(response);
 }
@@ -46,36 +47,42 @@ void setup(void) {
   DBG_OUTPUT_PORT.begin(115200);
   DBG_OUTPUT_PORT.setDebugOutput(true);
   DBG_OUTPUT_PORT.print("\n");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  //WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid, password);
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("EspServer", NULL);
+  dnsServer.start(53, "*", apIP);
+
   DBG_OUTPUT_PORT.print("Connecting to ");
   DBG_OUTPUT_PORT.println(ssid);
 
   // Wait for connection
   uint8_t i = 0;
-  while (WiFi.status() != WL_CONNECTED && i++ < 20) {//wait 10 seconds
-    delay(500);
-  }
-  if (i == 21) {
-    DBG_OUTPUT_PORT.print("Could not connect to");
-    DBG_OUTPUT_PORT.println(ssid);
-    while (1) delay(500);
-  }
+  //  while (WiFi.status() != WL_CONNECTED && i++ < 20) {//wait 10 seconds
+  //    delay(500);
+  //  }
+  //  if (i == 21) {
+  //    DBG_OUTPUT_PORT.print("Could not connect to");
+  //    DBG_OUTPUT_PORT.println(ssid);
+  //    while (1) delay(500);
+  //  }
   DBG_OUTPUT_PORT.print("Connected! IP address: ");
   DBG_OUTPUT_PORT.println(WiFi.localIP());
 
-  if (MDNS.begin(host)) {
-    MDNS.addService("http", "tcp", 80);
-    DBG_OUTPUT_PORT.println("MDNS responder started");
-    DBG_OUTPUT_PORT.print("You can now connect to http://");
-    DBG_OUTPUT_PORT.print(host);
-    DBG_OUTPUT_PORT.println(".local");
-  }
+//  if (MDNS.begin(host)) {
+//    MDNS.addService("http", "tcp", 80);
+//    DBG_OUTPUT_PORT.println("MDNS responder started");
+//    DBG_OUTPUT_PORT.print("You can now connect to http://");
+//    DBG_OUTPUT_PORT.print(host);
+//    DBG_OUTPUT_PORT.println(".local");
+//  }
 
 
   server.on("/", HTTP_GET, index_handler);
   server.on("/temp", HTTP_GET, temp_handler);
-  //server.onNotFound(handleNotFound);
+  server.onNotFound(index_handler);
 
   server.begin();
   DBG_OUTPUT_PORT.println("HTTP server started");
@@ -83,5 +90,5 @@ void setup(void) {
 }
 
 void loop(void) {
-
+  dnsServer.processNextRequest();
 }
