b064abdbe5ee2037f29d7a11d3337a1ed7424205
GelatoBaboom <idx001@gmail.com>
2020-05-25 20:35:14 -0300

    control de temp +app web

--- testGzipServer/testGzipServer.ino
+++ testGzipServer/testGzipServer.ino
@@ -6,22 +6,48 @@
 #include <ESPAsyncTCP.h>
 //#include <ESP8266mDNS.h>
 #include <DNSServer.h>
+#include "DallasTemperature.h"
+#include "OneWire.h"
+#include <SPI.h>
+#include <SD.h>
 #include "index.h"
+#include "jsgzip.h"
 IPAddress apIP(192, 168, 4, 1);
 #define DBG_OUTPUT_PORT Serial
+#define CS_PIN  D8
+#define ONE_WIRE_BUS_PIN  D1
 
 const char* ssid = "GelatoBaboom";
 const char* password = "friofrio";
 const char* host = "esp8266sd";
+uint32_t timerLoop;
 DNSServer dnsServer;
-AsyncWebServer  server = AsyncWebServer(80);
+File fi;
 
+OneWire oneWire(ONE_WIRE_BUS_PIN);
+DallasTemperature DS18B20(&oneWire);
+
+
+AsyncWebServer  server = AsyncWebServer(80);
+float getTemperature(uint8_t idx) {
+  //Serial.println("index: " + String(idx));
+  int cts = 10;
+  float temp;
+  do {
+    DS18B20.requestTemperatures();
+    temp = DS18B20.getTempCByIndex(idx);
+    delay(10);
+    cts--;
+  } while ((temp == 85.0 || temp == (-127.0)) && cts > 1);
+
+  return temp;
+}
 void temp_handler(AsyncWebServerRequest *request) {
   static char json_response[1024];
 
   char * p = json_response;
   *p++ = '{';
-  p += sprintf(p, "\"temp\":\"%.1f\"", 87);
+  p += sprintf(p, "\"temp\":\"%.1f\"", getTemperature(0));
   *p++ = '}';
   *p++ = 0;
 
@@ -29,9 +55,31 @@ void temp_handler(AsyncWebServerRequest *request) {
   response->addHeader("Access-Control-Allow-Origin", "*");
   request->send(response);
 }
+void temph_handler(AsyncWebServerRequest *request) {
+  static char json_response[1024];
+  DBG_OUTPUT_PORT.println("entra");
+  char * p = json_response;
+  fi = SD.open("values.txt");
+  if (fi) {
+    DBG_OUTPUT_PORT.println("esta, el archivo esta");
+    *p++ = '{\"values\":[';
+    while (fi.available()) {
+      String v = fi.readStringUntil('\n');
+      char buf[v.length() + 1];
+      strcpy(buf, v.c_str());
+      p += sprintf(p, "%.1f,", atof(buf) );
+    }
+    fi.close();
+    *p++ = ']}';
+    *p++ = 0;
+    DBG_OUTPUT_PORT.println(p);
+  }
+  AsyncWebServerResponse *response = request->beginResponse(200, "application/json", json_response);
+  response->addHeader("Access-Control-Allow-Origin", "*");
+  request->send(response);
+}
 
-
-void index_handler(AsyncWebServerRequest *request) {
+void index_handler(AsyncWebServerRequest * request) {
   // Dump the byte array in PROGMEM with a 200 HTTP code (OK)
   AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", index_gz, index_len);
 
@@ -42,7 +90,49 @@ void index_handler(AsyncWebServerRequest *request) {
   request->send(response);
 }
 
+void chartjs_handler(AsyncWebServerRequest * request) {
+  // Dump the byte array in PROGMEM with a 200 HTTP code (OK)
+  AsyncWebServerResponse *response = request->beginResponse_P(200, "text/javascript", chartjs_gz, chartjs_len);
 
+  // Tell the browswer the contemnt is Gzipped
+  response->addHeader("Content-Encoding", "gzip");
+  // And set the last-modified datetime so we can check if we need to send it again next time or not
+  //response->addHeader("Last-Modified", last_modified);
+  request->send(response);
+}
+
+void renderjs_handler(AsyncWebServerRequest * request) {
+  // Dump the byte array in PROGMEM with a 200 HTTP code (OK)
+  AsyncWebServerResponse *response = request->beginResponse_P(200, "text/javascript", renderjs_gz, renderjs_len);
+
+  // Tell the browswer the contemnt is Gzipped
+  response->addHeader("Content-Encoding", "gzip");
+  // And set the last-modified datetime so we can check if we need to send it again next time or not
+  //response->addHeader("Last-Modified", last_modified);
+  request->send(response);
+}
+
+void utilsjs_handler(AsyncWebServerRequest * request) {
+  // Dump the byte array in PROGMEM with a 200 HTTP code (OK)
+  AsyncWebServerResponse *response = request->beginResponse_P(200, "text/javascript", utils_gz, utils_len);
+
+  // Tell the browswer the contemnt is Gzipped
+  response->addHeader("Content-Encoding", "gzip");
+  // And set the last-modified datetime so we can check if we need to send it again next time or not
+  //response->addHeader("Last-Modified", last_modified);
+  request->send(response);
+}
+
+void jquerymin_handler(AsyncWebServerRequest * request) {
+  // Dump the byte array in PROGMEM with a 200 HTTP code (OK)
+  AsyncWebServerResponse *response = request->beginResponse_P(200, "text/javascript", jquerymin_gz, jquerymin_len);
+
+  // Tell the browswer the contemnt is Gzipped
+  response->addHeader("Content-Encoding", "gzip");
+  // And set the last-modified datetime so we can check if we need to send it again next time or not
+  //response->addHeader("Last-Modified", last_modified);
+  request->send(response);
+}
 void setup(void) {
   DBG_OUTPUT_PORT.begin(115200);
   DBG_OUTPUT_PORT.setDebugOutput(true);
@@ -50,13 +140,22 @@ void setup(void) {
   //WiFi.mode(WIFI_STA);
   //WiFi.begin(ssid, password);
 
-  WiFi.mode(WIFI_AP);
+  //  WiFi.mode(WIFI_AP);
+  //  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  //  WiFi.softAP("EspServer", NULL);
+
+  WiFi.mode(WIFI_AP_STA );
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP("EspServer", NULL);
+  WiFi.begin(ssid, password);
+
   dnsServer.start(53, "*", apIP);
 
   DBG_OUTPUT_PORT.print("Connecting to ");
   DBG_OUTPUT_PORT.println(ssid);
+  if (!SD.begin(10)) {
+    DBG_OUTPUT_PORT.println("initialization failed!");
+  }
 
   // Wait for connection
   uint8_t i = 0;
@@ -71,17 +170,22 @@ void setup(void) {
   DBG_OUTPUT_PORT.print("Connected! IP address: ");
   DBG_OUTPUT_PORT.println(WiFi.localIP());
 
-//  if (MDNS.begin(host)) {
-//    MDNS.addService("http", "tcp", 80);
-//    DBG_OUTPUT_PORT.println("MDNS responder started");
-//    DBG_OUTPUT_PORT.print("You can now connect to http://");
-//    DBG_OUTPUT_PORT.print(host);
-//    DBG_OUTPUT_PORT.println(".local");
-//  }
+  //  if (MDNS.begin(host)) {
+  //    MDNS.addService("http", "tcp", 80);
+  //    DBG_OUTPUT_PORT.println("MDNS responder started");
+  //    DBG_OUTPUT_PORT.print("You can now connect to http://");
+  //    DBG_OUTPUT_PORT.print(host);
+  //    DBG_OUTPUT_PORT.println(".local");
+  //  }
 
 
   server.on("/", HTTP_GET, index_handler);
   server.on("/temp", HTTP_GET, temp_handler);
+  server.on("/getJson.ashx", HTTP_GET, temph_handler);
+  server.on("/jquery.min.js", HTTP_GET, jquerymin_handler);
+  server.on("/chart.js", HTTP_GET, chartjs_handler);
+  server.on("/render.js", HTTP_GET, renderjs_handler);
+  server.on("/utils.js", HTTP_GET, utilsjs_handler);
   server.onNotFound(index_handler);
 
   server.begin();
@@ -91,4 +195,15 @@ void setup(void) {
 
 void loop(void) {
   dnsServer.processNextRequest();
+  //aca graba la temp en una funcioncon timer
+  //  if (((micros() - timerLoop) / 1000000) > 60)//(5 * 60))
+  //  {
+  //    fi = SD.open("values.txt", FILE_WRITE);
+  //    if (fi) {
+  //      float temperature1 = getTemperature(0);
+  //      fi.println( String(temperature1) );
+  //      fi.close();
+  //    }
+  //    timerLoop = micros();
+  //  }
 }
